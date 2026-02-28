47ea4e41cde662e6b41414e26878be2c0ded6571
Annie Peng <anniepeng888@gmail.com>
2026-02-28 14:18:35 -0600

    Sidebar

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,4 @@
+// handle popup/chat/backend requests
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "PROCESS_NEWS") {
     fetch("http://localhost:8000/process-news", {
@@ -10,11 +11,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     .then(res => res.json())
     .then(data => sendResponse({ success: true, data: data }))
     .catch(error => sendResponse({ success: false, error: error.message }));
-    
     // Return true to indicate we will send a response asynchronously
     return true;
   }
-
 
   if (request.type === "CHAT") {
     fetch("http://localhost:8000/chat", {
@@ -27,9 +26,23 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     .then(res => res.json())
     .then(data => sendResponse({ success: true, data: data }))
     .catch(error => sendResponse({ success: false, error: error.message }));
-    
     // Return true to indicate we will send a response asynchronously
     return true;
+  }
+});
+
+// when the user clicks the toolbar icon, either open the side panel (if supported)
+// or inject a floating sidebar into the current page
+chrome.action.onClicked.addListener((tab) => {
+  if (chrome.sidePanel && chrome.sidePanel.open) {
+    // open the built-in side panel provided by newer Chrome/Edge
+    chrome.sidePanel.open();
+  } else {
+    // fallback: inject a sidebar iframe into the page
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['injectSidebar.js']
+    });
   }
 });
 
